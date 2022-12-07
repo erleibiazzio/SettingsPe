8bd12c7fba690829cbeeb8573c418fb412a94e24
erleibiazzio <erleibiazzio@gmail.com>
2022-12-07 19:48:12 -0300

    Garante que o hook da subárea so rode no Agente

--- Plugin.php
+++ Plugin.php
@@ -15,12 +15,20 @@ class Plugin extends \MapasCulturais\Plugin
         $app = App::i();
         
         $app->view->enqueueStyle("app","settins-pe","css/settins-pe.css");
-        $app->hook("template(agent.<<edit|single>>.header-content):after",function(){
+        $app->hook("template(agent.<<edit|single>>.header-content):after",function() use ($app){
 
             /** @var Theme $this */
             $this->addTaxonoyTermsToJs("subarea");
             $this->addTaxonoyTermsToJs("tag");
+
+           
         });
+
+        $app->hook('view.partial(widget-tags).params', function ($data, &$template) {
+            if($this->controller->id == "agent"){
+                $template = "widget-tags-pe";
+            }
+         });
     }
 
     public function register()
